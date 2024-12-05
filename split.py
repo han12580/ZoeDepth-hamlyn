3d8e327cd7149590503efebe374ad2672b79a83b
han <3418728804@qq.com>
2024-12-05 19:52:59 +0800

    split

--- split.py
+++ split.py
@@ -1,6 +1,12 @@
 import os
 import random
+import argparse
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Personal information')
+
+    parser.add_argument('--test_id', type=int, help='Name of the test 0-9')
+    return parser.parse_args()
 # Function to read lines from a file
 def read_lines(filepath):
     with open(filepath, 'r') as file:
@@ -13,30 +19,25 @@ def write_lines(filepath, lines):
         for line in lines:
             file.write(line + '\n')
 
-# Paths to train and test files
-trainfilePath = "train_test_inputs/scared_train_files_with_gt.txt"
-testfilePath = "train_test_inputs/scared_test_files_with_gt.txt"
+def split(test_id = 0):
+    # Paths to train and test files
+    trainfilePath = "train_test_inputs/scared_train_files_with_gt.txt"
+    testfilePath = "train_test_inputs/scared_test_files_with_gt.txt"
+    allfilePath = "train_test_inputs/scared_all_files_with_gt.txt"
+    # Read all files
+    all_files = read_lines(allfilePath)
+    # split 10 parts and set test_id as test set
+    num_files = len(all_files)
+    num_test_files = int(num_files/10)
+    train_files = all_files[:num_test_files*test_id] + all_files[num_test_files*(test_id+1):]
+    test_files = all_files[num_test_files*test_id:num_test_files*(test_id+1)]
 
-# Read current train and test files
-train_files = read_lines(trainfilePath)
-test_files = read_lines(testfilePath)
+    # Write updated train and test files back to disk
+    write_lines(trainfilePath, train_files)
+    write_lines(testfilePath, test_files)
 
-# Determine the number of files to move between train and test sets
-num_train_to_val = int(0.2 * len(train_files))
-num_val_to_train = int(0.50 * len(test_files))
+    print("Dataset re-split done")
 
-# Shuffle files to ensure randomness
-random.shuffle(train_files)
-random.shuffle(test_files)
-
-# Move files from train to val and from val to train
-val_files = train_files[:num_train_to_val]
-train_files = train_files[num_train_to_val:]
-train_files.extend(test_files[:num_val_to_train])
-test_files = test_files[num_val_to_train:]
-
-# Write updated train and test files back to disk
-write_lines(trainfilePath, train_files)
-write_lines(testfilePath, test_files)
-
-print("Dataset re-split done")
+if __name__ == "__main__":
+    args = parse_args()
+    split(args.test_id)
